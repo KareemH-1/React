3d5b8937d31aa469dcbea2d5d5d073fa7783b5fa
KareemHassan <kareem99710@gmail.com>
2025-04-05 17:17:05 +0200

    Add package.json and package-lock.json for Credit Card Number Validator project
    - Created package.json to manage project dependencies, including the validator package.
    - Generated package-lock.json to lock the dependency versions for consistent installs.

--- Credit-Card-Number-Validator/Credit-Card-Number-Validator/src/App.jsx
+++ Credit-Card-Number-Validator/Credit-Card-Number-Validator/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from "react"; 
+import validator from 'validator'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+ 
+  const [errorMessage, setErrorMessage] = useState('') 
+    
+  const validateCreditCard = (value) => { 
+    
+    if (validator.isCreditCard(value)) { 
+      setErrorMessage('Valid CreditCard Number') 
+    } else { 
+      setErrorMessage('Enter valid CreditCard Number!') 
+    } 
+  } 
+  
+  return ( 
+    <div style={{ 
+      marginLeft: '200px', 
+    }}> 
+      <pre> 
+        <h2>Credit Card Validator</h2> 
+        <span>Enter CreditCard: </span><input type="text" 
+        onChange={(e) => validateCreditCard(e.target.value)}></input> <br /> 
+        <span style={{ 
+          fontWeight: 'bold', 
+          color: 'red', 
+        }}>{errorMessage}</span> 
+      </pre> 
+    </div> 
   )
 }
 
